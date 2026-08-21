cd1263bc7cf8c409e8c2eaf6fbc765587c62967c
Jayesh Sharma <574jayesh.sharma@gmail.com>
2026-08-21 11:58:24 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string s = "";
+
+        for(auto str : strs){
+            s += to_string(str.size()) + "#" + str;
+        }
+
+        return s;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> ans;
+
+        int i = 0;
+
+        while(i < s.size()){
+            string len = "";
+            while(s[i] != '#'){
+                len += s[i];
+                i++;
+            }
+
+            i++;
+
+            int k = stoi(len);
+
+            string str = s.substr(i,k);
+            ans.push_back(str);
+            i+= k;
+        }
+
+        return ans;
+    }
+};
